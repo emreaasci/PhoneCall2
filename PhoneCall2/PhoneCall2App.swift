cd73d4720ca726d392a13266cf15fbdf7531f5a5
emreasci <emreaasci@gmail.com>
2024-12-09 11:33:16 +0300

    Initial Commit

---
+++ PhoneCall2/PhoneCall2App.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneCall2App.swift
+//  PhoneCall2
+//
+//  Created by Emre Aşcı on 9.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhoneCall2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhoneCall2Tests/PhoneCall2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneCall2Tests.swift
+//  PhoneCall2Tests
+//
+//  Created by Emre Aşcı on 9.12.2024.
+//
+
+import Testing
+@testable import PhoneCall2
+
+struct PhoneCall2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
